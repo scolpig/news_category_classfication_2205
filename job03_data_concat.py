49ac732f7ff5d3a3b118e558b3305206d1a6dd02
scolpig <scolpig@gmail.com>
2022-05-26 13:08:02 +0900

    concat 02

--- job03_data_concat.py
+++ job03_data_concat.py
@@ -17,3 +17,16 @@ print(df['category'].value_counts())
 df.info()
 df.to_csv('./crawling_data/naver_news_titles_{}.csv'.format(
     datetime.datetime.now().strftime('%Y%m%d')), index=False)
+
+# 에러 없었을 때
+# df = pd.read_csv('./crawling_data/crawling_data.csv')
+# df_headline = pd.read_csv('./crawling_data/naver_headline_news_20220525.csv')
+# df_all = pd.concat([df, df_headline])
+#
+# print(df_all.head())
+# print(df_all.tail())
+# print(df_all['category'].value_counts())
+# df_all.info()
+# df_all.to_csv('./crawling_data/naver_news_titles_{}.csv'.format(
+#     datetime.datetime.now().strftime('%Y%m%d')), index=False)
+
